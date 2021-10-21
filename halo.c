66b308c558c9f3357724afad3d754d6813f2ca30
ivantendou <70643310+ivantendou@users.noreply.github.com>
2021-10-21 16:20:59 +0700

    Add files via upload

---
+++ halo.c
@@ -0,0 +1,19 @@
+/* File : halo.c */
+/* Penulis : Ivan, email ivantendou@gmail.com*/
+/* Program Halo Dunia */
+/* <stdio.h> adalah library standar untuk baca tulis*/
+#include <stdio.h>
+int
+main (void)
+{/* ini disebut badan program (body program)*/
+  printf("Halo Dunia!\n");
+  printf("Halo Dunia!\n");
+  printf("Halo Dunia!\n");
+  printf("Oh Baghdad, Oh Baghdad\n");
+  printf("Mengapa engkau menjadi suram\n");
+  printf("Harun Ar-Rasyid telah berpulang\n");
+  printf("Meninggalkan dua kubu\n");
+  printf("Satu Al-Amin, Dua Al-Makmun\n");
+  printf("Kurangnya komunikasi, mudahnya diprovokasi\n");
+  printf("Perang saudara pun akhirnya pecah\n");
+}
