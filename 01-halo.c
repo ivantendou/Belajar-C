b4537e70ca6a4c1d510f26b4dc71706564ca686f
ivantendou <70643310+ivantendou@users.noreply.github.com>
2021-10-21 16:30:50 +0700

    Update 01-halo.c

--- 01-halo.c
+++ 01-halo.c
@@ -1,11 +1,11 @@
 /* File : halo.c */
-/* Penulis : Ivan, email ivantendou@gmail.com*/
-/* Program Halo Dunia */
-/* <stdio.h> adalah library standar untuk baca tulis*/
+/* Penulis : Ivan, email ivantendou@gmail.com */
+/* Program untuk menuliskan Halo Dunia! ke layar */
+/* <stdio.h> adalah library standar untuk baca tulis */
 #include <stdio.h>
 int
 main (void)
-{/* ini disebut badan program (body program)*/
+{/* ini disebut badan program (body program) */
   printf("Halo Dunia!\n");
   printf("Halo Dunia!\n");
   printf("Halo Dunia!\n");
