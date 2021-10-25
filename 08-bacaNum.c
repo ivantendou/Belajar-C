620dadb107cc9e96ea156ec8015ba53f55aa8f6a
ivantendou <70643310+ivantendou@users.noreply.github.com>
2021-10-25 17:27:34 +0700

    Create 08-bacaNum.c

---
+++ 08-bacaNum.c
@@ -0,0 +1,22 @@
+/* File: bacaNum.c */
+/* Penulis : Ivan, email ivantendou@gmail.com */
+/* Deskripsi : */
+/* contoh membaca nilai numerik: bilangan bulat, bilangan riil */
+/* kemudian menuliskan nilai yang dibaca */
+#include <stdio.h>
+int main ()
+{
+    /* Kamus */
+    int a;
+    float x;
+    /* Program */
+    printf ("Contoh membaca dan menulis, ketik nilai integer: ");
+    scanf ("%d", &a); /* membaca nilai a yang bertype integer perhatikan bahwa nama variabel ditulis dg &a */
+    printf ("Nilai yang dibaca : %d \n", a);
+    
+    printf ("ketik nilai sebuah bilangan riil: ");
+    scanf ("%f", &x); /* membaca nilai x yang bertype float */
+    printf ("Nilai yang dibaca : %f \n", x);
+    
+    return 0;
+}
