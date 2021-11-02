2f2189a8d5529b29683d9a77509e30ec1f2903f4
ivantendou <70643310+ivantendou@users.noreply.github.com>
2021-11-02 18:31:27 +0700

    Create 18-for.c

---
+++ 18-for.c
@@ -0,0 +1,15 @@
+/* File: for.c */
+/* Penulis: Ivan, email ivantendou@gmail.com */
+/* Deskripsi: */
+/* print "Saya senang" sebanyak 3 kali */
+#include <stdio.h>
+int main ()
+{
+	/* Kamus : */
+	int i;
+	/* Algoritma */
+	for (i=0; i<3; i++) {
+		printf ("Saya senang belajar bahasa C \n");
+	}
+	return 0;
+}
