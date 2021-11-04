bd0eb9be41d5c1183aadc4024c103116bc3c7b40
ivantendou <70643310+ivantendou@users.noreply.github.com>
2021-11-04 20:53:22 +0700

    Create 23-tabel2.c

---
+++ 23-tabel2.c
@@ -0,0 +1,23 @@
+/* File: tabel2.c */
+/* Penulis: Ivan, email ivantendou@gmail.com */
+/* Deksripsi: */
+/* Mendefinisikan array dan mengisi nilainya dari input yang dibaca */
+#include <stdio.h>
+int main ()
+{
+    /* Kamus */
+    int Tab [5];
+    int i;
+    /* baca isi Tab */
+    for (i=0; i<5; i++) {
+        scanf ("%d", &Tab[i]);
+    }
+    
+    /* menuliskan isi Tab berderet ke kanan */
+    for (i=0; i<5 ; i++) {
+        printf ("Tab[%d]= %d ;", i, Tab[i]);
+    }
+    printf ("\n");
+    
+    return 0;
+}
