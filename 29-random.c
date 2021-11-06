59b0f8bdfe464760c5b4feccc564713cb8b5d629
ivantendou <70643310+ivantendou@users.noreply.github.com>
2021-11-06 16:37:11 +0700

    Create 29-random.c

---
+++ 29-random.c
@@ -0,0 +1,15 @@
+/* File: random.c */
+/* Penulis: Ivan, ivantendou@gmail.com */
+/* Deskripsi: */
+/* Pemakaian standard library untuk membangkitkan bilangan random */
+#include <stdio.h>
+#include <stdlib.h>
+/**********************/
+int main () {
+    const int MIN = 1;
+    const int MAX = 100;
+    /* Program akan membangkitkan 5 bilangan random antara MIN sampai MAX */
+    for (int i = 0; i<5; i++)
+        printf (" %d ", rand ()% MAX + MIN);
+    return 0;
+}
